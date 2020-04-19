a79603587e590250314663d1d65a61751fb73ab1
Salaka Navinda <mmsnavinda@gmail.com>
2020-04-19 08:00:04 +0530

    Initiated Project

---
+++ SourceCode/com-group-filter/src/main/java/com/groupby/filter/com_group_filter/App.java
@@ -0,0 +1,13 @@
+package com.groupby.filter.com_group_filter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
